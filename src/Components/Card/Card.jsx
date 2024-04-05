4dab21892efee3362c8ad758fbbcf79fed4449aa
ashish garg <ashish.garg@metamerch.in>
2024-04-05 20:42:19 +0530

    improvment: fix allignment for product card

--- src/Components/Card/Card.jsx
+++ src/Components/Card/Card.jsx
@@ -7,11 +7,13 @@ function Card({
 }) {
   return (
     <div className="card mt-3">
+      <div className='flex1'>
       <div className="card-inner">
           <img src={image} alt="img" style={{width: '100px', height: '70px' , scale: '0.8',aspectRatio: '1/1',borderRadius:"50px"}}  />
       </div>
       <div className="product-name">{name}</div>
       <div className="product-description">{description}</div>
+      </div>
       <div className="price mt-3">
         <span className='Value'>₹ {price}/-</span>
         <span><button className="add-btn">Add</button></span>
